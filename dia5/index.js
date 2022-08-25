e7ae1f65dcc9392b3bb6db78738bca3656fb4bed
SILVA94 <ric.silva15@gmail.com>
2022-08-24 20:08:03 -0500

    Actualizando codigo día5 antes del break

--- dia5/index.js
+++ dia5/index.js
@@ -1,30 +1,50 @@
 const formulario = document.getElementById('formulario')
 
-const dbUser = 'Migue'
+const dbUser = 'Chuy'
 const dbPassword = '1234abc'
 
+const crear = (user) => {
+  let span = document.createElement('span')
+  span.classList.add('lista')
+  span.innerHTML = ` 
+  Bienvenido ${user}
+
+  `
+  let render= document.getElementById('render')
+  render.appendChild(span)
+}
+
+//` ` 
+function  errores(tipo){
+  let error = document.getElementById(`error${tipo}`)
+  error.classList.remove('hide')
+  error.classList.add('error')
+  setTimeout(function(){
+    error.classList.remove('error')
+    error.classList.add('hide')
+  },5000)
+}
 function validar(u,p){
    if (u === ''  || p === ''){
       console.log('No ingresaste usuario o password. Ingresa un dato')
+      console.log('Bienvenido a tu cuenta')
+      errores('Datos')
    } 
    else {
       if( u === dbUser && p === dbPassword){
-         console.log('Bienvenido a tu cuenta')
+         console.log('Bienvenido a la cuenta')
+         crear(dbUser)
        
       //else {
         // console.log('Usuario o password incorrecto')
      // }
       } 
       else if (u != dbUser){
-         let errorUser = document.getElementById('errorUsuario')
-         errorUser.classList.remove('hide')
-         errorUser.classList.add('error')
+         errores('Usuario')
          
       } 
       else if (p != dbPassword){
-         let errorPassword = document.getElementById('errorPassword')
-         errorPassword.classList.remove('hide')
-         errorPassword.classList.add('error')
+         errores('Password')
       }
    }
 }
@@ -35,4 +55,5 @@ formulario.addEventListener('submit', function(evento){
    let usuario = document.getElementById('usuario').value
    let password = document.getElementById('password').value
    validar(usuario,password)
+   
 })
